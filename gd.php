d9315f2cd4247fdd02939780e4bd504296fe969a
Kanade <andy_wang0625@hotmail.com>
2019-11-29 16:22:29 -0500

    Init Commit to Playground

---
+++ gd.php
@@ -0,0 +1,8 @@
+<?php
+    ob_start();
+    $imgRes = imagecreatefromjpeg("./imgs/1.jpeg");
+    imagejpeg($imgRes);
+    $imgBuffer = ob_get_clean();
+    $base64Img = base64_encode($imgBuffer);
+?>
+<img style="width:25%;" src="data:image/jpeg;base64,<?php echo($base64Img); ?>" />
