bfa07195a55a824970a2358dc5a1af24c9a634f1
Kanade <andy_wang0625@hotmail.com>
2019-11-29 16:29:08 -0500

    Added jQuery CDN

--- gd.php
+++ gd.php
@@ -1,4 +1,5 @@
 <?php
+    include_once("./Lab5Common/Header.php");
     ob_start();
     $imgRes = imagecreatefromjpeg("./imgs/1.jpeg");
     imagejpeg($imgRes);
@@ -6,3 +7,6 @@
     $base64Img = base64_encode($imgBuffer);
 ?>
 <img style="width:25%;" src="data:image/jpeg;base64,<?php echo($base64Img); ?>" />
+
+
+<?php include_once("./Lab5Common/Footer.php"); ?>
