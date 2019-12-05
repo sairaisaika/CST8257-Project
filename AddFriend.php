fb07bd4865f4805e218dad987c7106b4b7dbc86b
Kanade <andy_wang0625@hotmail.com>
2019-12-05 00:51:35 -0500

    Fixed Bugs

--- AddFriend.php
+++ AddFriend.php
@@ -3,61 +3,61 @@
     include_once './src/Lab5Common/Connection.php';
     include_once './src/Lab5Common/Functions.php';
     include_once './src/Lab5Common/Footer.php';
-    
+
     if(!isset($_SESSION['userid'])){
         header("Location: login.php");
     }
-    
-    $friendId = $_POST["friendId"];
-    $_SESSION['friendId'] = $_POST["friendId"];
-    
+    if(isset($_POST['friendId'])){
+        $friendId = $_POST["friendId"];
+        $_SESSION['friendId'] = $_POST["friendId"];
+    }
     $friendError ="";
-    
+
     $sql = "SELECT Name from User WHERE UserId=:UserId";
     $pStmt = $myPdo->prepare($sql);
     $pStmt->execute([':UserId' => $_SESSION["userid"]]);
     $printName = $pStmt->fetch();
-    
+
     if(isset($_POST['sendRequest'])){
         $sql = "SELECT * FROM User WHERE UserId = :userId";
         $pStmt = $myPdo->prepare($sql);
         $pStmt->execute([':userId' => $friendId]);
         $chekId = $pStmt->fetch();
-        
+
         $sql= "SELECT * FROM Friendship"
                 . "WHERE Friend_RequesterId = :requestrId AND Friend_RequesteeId = :requesteeId AND Status = :status";
         $pStmt = $myPdo->prepare($sql);
         $pStmt->execute(array(':requestrId' => $_SESSION['userid'], ':requesteeId' => $_SESSION['friendId'], 'status' => 0));
         $request1 = $pStmt->fetch();
-        
+
         $sql= "SELECT * FROM Friendship"
                 . "WHERE Friend_RequesterId = :requestrId AND Friend_RequesteeId = :requesteeId AND Status = :status";
         $pStmt = $myPdo->prepare($sql);
         $pStmt->execute(array(':requestrId' => $_SESSION['friendId'], ':requesteeId' => $_SESSION['userid'], 'status' => 0));
         $request2 = $pStmt->fetch();
-        
+
         $sql = "SELECT * FROM Friendship"
                 . "WHERE Friend_RequesterId = :friendId AND Friend_RequesteeId = :userId AND Status = :status";
         $pStmt =$myPdo->prepare($sql);
         $pStmt->execute(array(':userId' => $_SESSION['userid'], ':friendId' => $_SESSION['friendId'], ':status' => 1));
         $pending = $pStmt->fetch();
-        
+
         $sql = "SELECT * FROM Friendship"
                 . "WHERE Friend_RequesterId = :userId AND Friend_RequesteeId = :friendId AND Status = :status";
         $pStmt =$myPdo->prepare($sql);
         $pStmt->execute(array(':userId' => $_SESSION['userid'], ':friendId' => $_SESSION['friendId'], ':status' => 1));
         $pendingFriend = $pStmt->fetch();
-        
-        
+
+
         if($pendingFriend != null){
             $friendError = "You can not send an invitation twice";
         }
         else{
             $sql = "SELECT UserId, Name FROM User WHERE UserId = :friendId";
-            $pStmt = $myPdo->prepare($sql);        
-            $pStmt ->execute([':friendId' => $_SESSION['friendId']]);  
+            $pStmt = $myPdo->prepare($sql);
+            $pStmt ->execute([':friendId' => $_SESSION['friendId']]);
             $id = $pStmt->fetch();
-            
+
             if($chekId == null){
                 $friendError = "There is no User with the ID!";
             }
@@ -73,13 +73,13 @@
                 $pStmt = $myPdo->prepare($sql);
                 $pStmt->execute(array(':requesterId' => $_SESSION['userid'], ':requesteeId' => $_SESSION['friendId']));
                 $pStmt->commit;
-                
+
                 $sql = "INSERT INTO Friendship(Friend_RequesterId, Friend_RequesteeId, Status)"
                         . "VALUES(:requesterId, :requesteeId, :status)";
                 $pStmt = $myPdo->prepar($sql);
                 $pStmt->execute(array(':requesterId' => $_SESSION['userid'], ':requesteeId' => $_SESSION['friendId'], ':status' => 0));
                 $pStmt->commit;
-                
+
                 $friendError = "You and ".$id[1]." (ID:".$id[0].") are now friends.";
             }
             else{
@@ -88,7 +88,7 @@
                 $pStmt = $myPdo->prepare($sql);
                 $pStmt->execute(array(':requesterId' => $_SESSION['userid'], ':requesteeId' => $_SESSION['friendId'], ':status' => 1));
                 $pStmt->commit;
-                
+
                 $friendError = "Your request was sent to ".$id[1]." (ID:".$id[0].")."
                         ."<br>"
                         ."Once ".$id[1]." accepts your request, you and ".$id[1]." will be friends"
@@ -98,19 +98,19 @@
     }
 ?>
     <div class="container">
-        <h1> Add Friend</h1>  
+        <h1> Add Friend</h1>
         <br>
             <h4> Welcome <b><?php print $printName[0];?></b>! (Not you? Change your session <a href="Login.php">here</a>)</h4>
             <h4> Enter the ID of the user you want to be friends with:</h4>
 
-        <form method='post' action=AddFriend.php>             
+        <form method='post' action=AddFriend.php>
             <br><br><div class="row">
                 <div class="col-lg-1" >
                     <label for='friendId' class='col-form-label'><b>ID:</b> </label>
                 </div>
                 <div class="col-lg-3" >
                     <input type='text' class='form-control' id='friendId' name='friendId' value='<?php print $_SESSION['friendId']; ?>' >
-                </div> 
+                </div>
                 <br>
                 <div class="col-lg-5" >
                     <button type='submit' name='sendRequest' class='btn btn-primary'>Send Friend Request</button>
@@ -118,5 +118,4 @@
                 <br><div class='col-lg-10' style='color:red'> <?php print $friendError;?></div>
             </div>
         </form>
-    </div>   
-
+    </div>
--- MyAlbums.php
+++ MyAlbums.php
@@ -3,18 +3,18 @@
     include_once './src/Lab5Common/Connection.php';
     include_once './src/Lab5Common/Functions.php';
     include_once './src/Lab5Common/Footer.php';
-    
+
     if(!isset($_SESSION['userid'])){
         header("Location: login.php");
     }
-    
+
     $sql = "SELECT Name from User WHERE UserId=:UserId";
     $pStmt = $myPdo->prepare($sql);
     $pStmt->execute([':UserId' => $_SESSION["userid"]]);
     $printName = $pStmt->fetch();
-    
+
     $accessibilityArray = $_SESSION['accessibilityArray'];
-    
+
     $sql = "SELECT a.Title, a.Date_Updated, ac.Description, a.Album_Id, COALESCE(picture, 0) number_pictures "
             . "FROM Album a "
             . "LEFT JOIN (SELECT count(*) as Picture, Album_Id FROM Picture GROUP BY Album_Id) p ON a.Album_Id = p.Album_Id "
@@ -23,32 +23,32 @@
     $pStmt = $myPdo->prepare($sql);
     $pStmt->execute ( [':userId' => $_SESSION['userid']] );
     $albumByUser = $pStmt->fetchAll();
-    
-    $sql = "SELECT * FROM Accessibility";    
-    $pStmt = $myPdo->prepare($sql); 
+
+    $sql = "SELECT * FROM Accessibility";
+    $pStmt = $myPdo->prepare($sql);
     $pStmt->execute();
-    
+    if(isset($_GET['action']))
     if ($_GET['action']== 'delete' && isset($_GET['id'])){
-        $ID = $_GET['id'];  
-        $deletePictures = "DELETE FROM Picture WHERE Album_Id = :albumID";            
+        $ID = $_GET['id'];
+        $deletePictures = "DELETE FROM Picture WHERE Album_Id = :albumID";
         $stmt = $myPdo->prepare($deletePictures);
         $delalbum = "DELETE FROM Album WHERE Album.Album_Id = :albumID";
         $stmt1 = $myPdo->prepare($delalbum);
         $stmt->execute([':albumID' => $ID]);
         $stmt1->execute([':albumID' => $ID]);
-        
+
         header('Location: MyAlbums.php');
-        exit; 
-    }  
-    
+        exit;
+    }
+
     $accessibilityArray = null;
     foreach ($pStmt as $row)
     {
-        $accessibility = array( $row['Accessibility_Code'], $row['Description'] ); 
+        $accessibility = array( $row['Accessibility_Code'], $row['Description'] );
         $accessibilityArray[] = $accessibility;
     }
     $_SESSION['accessibilityArray'] = $accessibilityArray;
-    
+
     if(isset($_POST['submit'])){
         if(isset($_POST['selectAcessibility'])){
             $sql = "UPDATE Album SET Accessibility_Code = :access_code WHERE Album_Id = :album_id";
@@ -71,12 +71,12 @@
 
         <form method='post' action='MyAlbums.php'>
             <br><br><br>
-            <div class='row'>               
+            <div class='row'>
                 <div class='col-lg-10 col-md-9 col-sm-9 col-xs-7'></div>
                 <div class='col-lg-2 col-md-3 col-sm-3 col-xs-5'>
                     <b><a href="AddAlbum.php">Create a New Album</a></b>
                 </div>
-            </div>  
+            </div>
             <table class="table">
                 <thead>
                     <tr>
@@ -84,9 +84,9 @@
                         <th scope="col">Date Updated</th>
                         <th scope="col">Number of Pictures</th>
                         <th scope="col">Accessibility</th>
-                        <th scope="col"></th>                                                                              
+                        <th scope="col"></th>
                     </tr>
-                </thead>   
+                </thead>
 
                 <tbody>
                     <?php
@@ -98,29 +98,28 @@
                         echo "<td scope='col'>". $var[4] . "</td>";
                         echo "<td scope='col'><select name='selectAcessibility[]' class='form-control' >  ";
                         foreach ($accessibilityArray as $row)
-                        {   
+                        {
                             echo "<option value='$row[0]' ";
                             if ($row[1] == $var[2])
-                                { 
+                                {
                                     echo "selected='selected'";
                                 }
                             echo ">" . $row[1] . "</option>";
-                        }          
+                        }
                         echo "</select>";
                         echo "<td scope='col'><a href='MyAlbums.php?action=delete&id=$var[3]' onclick='return myFunctionDelete()'/a>Delete</td>"; // delete button
                         echo "</tr>";
-                    }                              
+                    }
                     ?>
                 </tbody>
             </table>
 
             <br>
-            <div class='row'>               
+            <div class='row'>
                 <div class='col-lg-9 col-md-9 col-sm-8 col-xs-6'></div>
                 <div class='col-lg-2 col-md-2 col-sm-3 col-xs-6'>
-                    <button type='submit' name='submit' class='btn btn-block btn-primary'>Save Changes</button>  
-                </div> 
-            </div>  
-    </form>   
+                    <button type='submit' name='submit' class='btn btn-block btn-primary'>Save Changes</button>
+                </div>
+            </div>
+    </form>
 </div>
-
