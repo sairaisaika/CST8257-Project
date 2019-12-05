d969a045f2e325dff3d703caae0c668d02019d55
Rathru3704 <chan0433@algonquinlive.com>
2019-12-05 14:06:23 -0500

    Added Feature/UploadImage

--- src/Lab5Common/functions.php
+++ src/Lab5Common/functions.php
@@ -46,6 +46,30 @@ function ValidatePassword($password){
     return True;
 }
 
+function convertImage($originalImage, $outputImage, $quality)
+{
+    // jpg, png, gif or bmp?
+    $exploded = explode('.',$originalImage);
+    $ext = $exploded[count($exploded) - 1]; 
+
+    if (preg_match('/jpg|jpeg/i',$ext))
+        $imageTmp=imagecreatefromjpeg($originalImage);
+    else if (preg_match('/png/i',$ext))
+        $imageTmp=imagecreatefrompng($originalImage);
+    else if (preg_match('/gif/i',$ext))
+        $imageTmp=imagecreatefromgif($originalImage);
+    else if (preg_match('/bmp/i',$ext))
+        $imageTmp=imagecreatefrombmp($originalImage);
+    else
+        return 0;
+
+    // quality is a value from 0 (worst) to 100 (best)
+    imagejpeg($imageTmp, $outputImage, $quality);
+    imagedestroy($imageTmp);
+
+    return 1;
+}
+
 function save_uploaded_file($destinationPath)
 {
 	if (!file_exists($destinationPath))
